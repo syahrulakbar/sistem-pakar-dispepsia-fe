b5b21277c4e78b8260de31a904d5b12fa31937cc
Mochamad Syahrul Akbar <07tav2akbar@gmail.com>
2024-04-10 20:34:36 +0700

    feat: add dashboard sipardi

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,43 @@
+import { useEffect, useState } from "react";
+import AxiosJWTConfig from "../utils/axiosJWT";
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const [data, setData] = useState({});
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const axiosJWT = await AxiosJWTConfig();
+        const response = await axiosJWT.get(`/sipardi`);
+        setData(response.data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <>
+      <h1 className="font-bold text-2xl">Dashboard</h1>
+      <p className="text-sm">Sistem Pakar Diagnosa Dispepsia</p>
+      <div className="mt-5 flex w-full flex-col xl:flex-row gap-5 flex-wrap">
+        <div className="border-black border-2 rounded-md text-center w-full xl:w-1/3 h-36 flex items-center justify-center flex-col">
+          <h2 className="font-bold text-xl">Total Pengguna</h2>
+          <p className="text-3xl">{data?.user || "Loading"}</p>
+        </div>
+        <div className="border-black border-2 rounded-md text-center w-full xl:w-1/3 h-36 flex items-center justify-center flex-col">
+          <h2 className="font-bold text-xl">Data Penyakit</h2>
+          <p className="text-3xl">{data?.penyakit || "Loading"}</p>
+        </div>
+        <div className="border-black border-2 rounded-md text-center w-full xl:w-1/3 h-36 flex items-center justify-center flex-col">
+          <h2 className="font-bold text-xl">Data Gejala</h2>
+          <p className="text-3xl">{data?.gejala || "Loading"}</p>
+        </div>
+        <div className="border-black border-2 rounded-md text-center w-full xl:w-1/3 h-36 flex items-center justify-center flex-col">
+          <h2 className="font-bold text-xl">Data Rule</h2>
+          <p className="text-3xl">{data?.rules || "Loading"}</p>
+        </div>
+      </div>
+    </>
+  );
 }
